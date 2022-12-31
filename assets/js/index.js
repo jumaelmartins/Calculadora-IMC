c9c69b6c5e759b063e0020c631b02bc82f84274f
jumaelmartins <jumaelmartins@gmail.com>
2022-12-31 20:40:00 -0300

    Ajustes visuais

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,6 +3,7 @@ function calcularImc() {
   let alturaValue = document.querySelector("#alturaValue");
   const pesoInput = document.querySelector(".pesoInput");
   let pesoValue = document.querySelector("#pesoValue");
+  let inputTermometro = document.querySelector(".termometro");
 
   alturaInput.addEventListener("input", (e) => {
     return (alturaValue.innerHTML = e.target.value / 100).toFixed(2);
@@ -13,48 +14,43 @@ function calcularImc() {
   });
 
   const form = document.querySelector(".form");
-  const results = document.querySelector(".results");
+  let results = document.querySelector(".results");
+
+  let p = document.createElement('p')
+
+  //  results.appendChild(Termometro);
 
   form.addEventListener("submit", (e) => {
     e.preventDefault();
-    console.log("submit");
-    //   const altura = parseFloat(form.querySelector('.altura').value);
-    //   const peso = parseFloat(form.querySelector('.peso').value);
+  //  console.log("submit");
+  //  console.log(typeof pesoValue.innerText, typeof alturaValue.innerText);
+    
 
-    console.log(typeof pesoValue.innerText, typeof alturaValue.innerText);
-
-    const imc = pesoValue.innerText / (alturaValue.innerText * alturaValue.innerText);
+    const imc =
+      Number(pesoValue.innerText / (alturaValue.innerText * alturaValue.innerText));
 
     const imcResults = (imc) => {
       if (!imc) {
-        results.classList.remove("blue");
-        results.classList.remove("orange");
-        results.classList.add("red");
         return "valor invalido";
       } else if (imc < 18.5) {
-        results.classList.remove("blue");
-        results.classList.remove("orange");
-        results.classList.add("red");
         return `seu imc é ${imc.toFixed(2)} o seu peso está abaixo do normal`;
       } else if (imc >= 18.5 && imc < 24.9) {
-        results.classList.remove("red");
-        results.classList.remove("orange");
-        results.classList.add("blue");
         return `seu imc é ${imc.toFixed(2)} o seu está peso normal`;
       } else if (imc >= 24.9 && imc < 30) {
-        results.classList.remove("blue");
-        results.classList.remove("orange");
-        results.classList.add("orange");
         return `seu imc é ${imc.toFixed(2)} e vôce está com sobrepeso`;
       } else if (imc > 30) {
-        results.classList.remove("blue");
-        results.classList.remove("orange");
-        results.classList.add("red");
         return `seu imc é ${imc.toFixed(2)} e você está obeso`;
       }
     };
+    inputTermometro.value = Math.round(imc);
+    p.innerHTML = imcResults(imc)
 
-    results.innerHTML = `<p>${imcResults(imc)}</p>`;
+    results.classList.remove('hidden')
+
+    try {
+    results = results.appendChild(p);
+    } catch { console.log('algo errado não está certo')}
+
   });
 }
 
