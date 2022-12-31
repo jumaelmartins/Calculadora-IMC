9486fad411381b38a5af407a6e00cc92ec72f8a4
jumaelmartins <jumaelmartins@gmail.com>
2022-12-31 16:09:45 -0300

    Modificação no Layout

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,52 +1,61 @@
+function calcularImc() {
+  const alturaInput = document.querySelector(".alturaInput");
+  let alturaValue = document.querySelector("#alturaValue");
+  const pesoInput = document.querySelector(".pesoInput");
+  let pesoValue = document.querySelector("#pesoValue");
 
+  alturaInput.addEventListener("input", (e) => {
+    return (alturaValue.innerHTML = e.target.value / 100).toFixed(2);
+  });
 
-function calcularImc () {
+  pesoInput.addEventListener("input", (e) => {
+    return (pesoValue.innerHTML = parseFloat(e.target.value));
+  });
 
-    const form = document.querySelector('.form')
-    const results = document.querySelector('.results')
+  const form = document.querySelector(".form");
+  const results = document.querySelector(".results");
 
-    form.addEventListener ('submit', (e) => {
-        e.preventDefault();
-        console.log('submit');
-        const altura = parseFloat(form.querySelector('.altura').value);
-        const peso = parseFloat(form.querySelector('.peso').value);
-        
-        console.log(typeof(peso), typeof(altura))
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log("submit");
+    //   const altura = parseFloat(form.querySelector('.altura').value);
+    //   const peso = parseFloat(form.querySelector('.peso').value);
 
-        const imc = peso / (altura * altura);  
-        
-        const imcResults = (imc) => {
+    console.log(typeof pesoValue.innerText, typeof alturaValue.innerText);
 
-            if (!imc) {
-                results.classList.remove('blue')
-                results.classList.remove('orange')
-                results.classList.add('red')
-                return 'valor invalido'
-            } else if (imc <18.5) {
-                results.classList.remove('blue')
-                results.classList.remove('orange')
-                results.classList.add('red')
-                return `seu imc é ${imc.toFixed(2)} o seu peso está abaixo do normal`
-            } else if ( imc >= 18.5 && imc <24.9) {
-                results.classList.remove('red')
-                results.classList.remove('orange')
-                results.classList.add('blue')
-                return `seu imc é ${imc.toFixed(2)} o seu está peso normal`
-            } else if ( imc >= 24.9 && imc <30) {
-                results.classList.remove('blue')
-                results.classList.remove('orange')
-                results.classList.add('orange')
-                return `seu imc é ${imc.toFixed(2)} e vôce está com sobrepeso`
-            } else if ( imc >30) {
-                results.classList.remove('blue')
-                results.classList.remove('orange')
-                results.classList.add('red')
-                return `seu imc é ${imc.toFixed(2)} e você está obeso`
-            }
-        }
+    const imc = pesoValue.innerText / (alturaValue.innerText * alturaValue.innerText);
 
-        results.innerHTML = (`<p>${imcResults(imc)}</p>`)
-    })
-};
+    const imcResults = (imc) => {
+      if (!imc) {
+        results.classList.remove("blue");
+        results.classList.remove("orange");
+        results.classList.add("red");
+        return "valor invalido";
+      } else if (imc < 18.5) {
+        results.classList.remove("blue");
+        results.classList.remove("orange");
+        results.classList.add("red");
+        return `seu imc é ${imc.toFixed(2)} o seu peso está abaixo do normal`;
+      } else if (imc >= 18.5 && imc < 24.9) {
+        results.classList.remove("red");
+        results.classList.remove("orange");
+        results.classList.add("blue");
+        return `seu imc é ${imc.toFixed(2)} o seu está peso normal`;
+      } else if (imc >= 24.9 && imc < 30) {
+        results.classList.remove("blue");
+        results.classList.remove("orange");
+        results.classList.add("orange");
+        return `seu imc é ${imc.toFixed(2)} e vôce está com sobrepeso`;
+      } else if (imc > 30) {
+        results.classList.remove("blue");
+        results.classList.remove("orange");
+        results.classList.add("red");
+        return `seu imc é ${imc.toFixed(2)} e você está obeso`;
+      }
+    };
+
+    results.innerHTML = `<p>${imcResults(imc)}</p>`;
+  });
+}
 
 calcularImc();
